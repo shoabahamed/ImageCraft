ef2fe624e1d642482fec56af9ff7696c2e671bbf
Shoab <shoabahamed2007015@gmail.com>
2024-11-28 12:04:23 +0600

    fixed merged confilit in master

--- client/src/components/ui/label.tsx
+++ client/src/components/ui/label.tsx
@@ -1,17 +1,12 @@
-<<<<<<< HEAD
-=======
-"use client"
+import * as React from "react";
+import * as LabelPrimitive from "@radix-ui/react-label";
+import { cva, type VariantProps } from "class-variance-authority";
 
->>>>>>> 8e83f5759720370913f6a75126310cf0810e5024
-import * as React from "react"
-import * as LabelPrimitive from "@radix-ui/react-label"
-import { cva, type VariantProps } from "class-variance-authority"
-
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
 
 const labelVariants = cva(
   "text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-)
+);
 
 const Label = React.forwardRef<
   React.ElementRef<typeof LabelPrimitive.Root>,
@@ -23,7 +18,7 @@ const Label = React.forwardRef<
     className={cn(labelVariants(), className)}
     {...props}
   />
-))
-Label.displayName = LabelPrimitive.Root.displayName
+));
+Label.displayName = LabelPrimitive.Root.displayName;
 
-export { Label }
+export { Label };

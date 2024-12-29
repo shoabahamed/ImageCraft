67f503fec1f2775c6c8fcced49e15ad74cb55887
Shoab <shoabahamed2007015@gmail.com>
2024-12-30 02:18:10 +0600

    submitted and loaded image for each user

--- server/main.py
+++ server/main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 from flask_cors import CORS
 from routes.auth_routes import auth_routes
 from routes.project_routes import project_routes
@@ -9,9 +9,17 @@ load_dotenv()
 
 app = Flask(__name__)
 app.secret_key = os.getenv("APP_SECRET")
-CORS(app, origins="*", supports_credentials=True, 
-     allow_headers=["Authorization", "Content-Type", "Access-Control-Allow-Headers"], 
-     expose_headers=["Authorization"])
+# CORS(app, origins="*", supports_credentials=True, 
+#      allow_headers=["Authorization", "Content-Type", "Access-Control-Allow-Headers"], 
+#      expose_headers=["Authorization"])
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'C:/Shoab/PROJECTS/StyleForge/static/uploads'
+
+@app.route('/static/uploads/<string:filename>')
+def download_file(filename):
+    print(filename)
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename, as_attachment=False)
 
 # Register Blueprints
 app.register_blueprint(auth_routes)
